245df94c1b6d65ebec7f10fd991529e9c580a591
Rocard Fonji <fonkengrocard@gmail.com>
2025-06-17 00:36:22 +0100

    feat: add todo model and types

---
+++ todo-backend/src/types/todo.ts
@@ -0,0 +1,10 @@
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  created_at: string;
+  updated_at: string;
+}
+
+export type CreateTodoDto = Pick<Todo, 'title'> & Partial<Pick<Todo, 'completed'>>;
+export type UpdateTodoDto = Partial<CreateTodoDto>;
