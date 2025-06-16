6ddfef0a72bf381ea2f5057a7a7a0f2c8dc267b7
Rocard Fonji <fonkengrocard@gmail.com>
2025-06-16 23:43:25 +0100

    feat: setup wip express server

---
+++ todo-backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
