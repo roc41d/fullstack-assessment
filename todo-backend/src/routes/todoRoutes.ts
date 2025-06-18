75bfb7b1a4a365d02c76ea8991d31eba29bef7b6
Rocard Fonji <fonkengrocard@gmail.com>
2025-06-18 07:50:44 +0100

    fix: clear completed endpoint

--- todo-backend/src/routes/todoRoutes.ts
+++ todo-backend/src/routes/todoRoutes.ts
@@ -9,7 +9,7 @@ router.get('/:id', TodoController.show);
 router.post('/', TodoController.create);
 router.put('/:id/complete', TodoController.completeTodo);
 router.put('/:id', TodoController.update);
-router.delete('/:id', TodoController.remove);
 router.delete('/completed', TodoController.clearCompleted);
+router.delete('/:id', TodoController.remove);
 
 export default router;
